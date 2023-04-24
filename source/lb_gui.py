16af19a2598e61885edb44e911b44a15ba271726
Milad-Mrz <mirzaei.milad.m@gmail.com>
2023-04-24 22:20:25 +0200

    0.0.1

--- source/lb_gui.py
+++ source/lb_gui.py
@@ -22,34 +22,72 @@ from lb_main import *
 
 
 class BladeLearner2049(App):
-    def build(self): 
+    def build(self):  
+        self.white = (1, 1, 1, 1)
+        self.study_options  = []
 
-
-        
         def gui_home():
+            
+            #self.study_options  = mainMapReduce()
             # setting  main window features
+            home = FloatLayout()
 
-            study_options  = mainMapReduce()
-            self.study_options = study_options
+            #create central line
+            center_line = Label(text='____________________________________________',
+                                font_size ='18sp', size_hint=(0.2, 0.2),
+                                pos_hint={'center_x': 0.5, 'center_y': 0.5})
+            center_line.color = (1,1,1,1)
 
+            study_btn = Button(text='Study Cards', 
+                        background_color=(1, 1, 1, 0),
+                        color=self.white,
+                        size_hint=(0.4, 0.1),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.6},
+                        font_name='Allura_Regular',
+                        font_size=45)
+            
+            manage_btn = Button(text='Manage Cards', 
+                        background_color=(1, 1, 1, 0),
+                        color=self.white,
+                        size_hint=(0.4, 0.1),
+                        pos_hint={'center_x': 0.5, 'center_y': 0.45},
+                        font_name='Allura_Regular',
+                        font_size=20)
+            
             # setting a popup window on top for home page and buttons
+            study_btn.bind(on_press= gui_category)
+            #manage_btn.bind(on_press= gui_manage)
+            
+            home.add_widget(center_line)
+            home.add_widget(study_btn)
+            home.add_widget(manage_btn)
 
-            study_btn.bind(on_press= gui_Category())
-            manage_btn.bind(on_press= gui_manage())
-
-
+            popup.content = home
+            popup.open()
         
-        def gui_category():
+
+        def gui_category(instance):
             # close last pop 
+            popup.dismiss()
+
             # setting a popup window on top for the page and buttons
             # create a grid of all topics and let user choose
 
+            for options in self.study_options :  #input list
+                print(options)
 
-            self.study_options #input list
-            self.chosen_category = chosen_category # output
-            gui_study_type()
+            #self.chosen_category = chosen_category # output
+            #gui_study_type()
 
-            btn_return.bind(on_press= gui_home(self.study_options))
+            #btn_return.bind(on_press= gui_home(self.study_options))
+
+            category = FloatLayout()
+
+            popup.content = category
+            popup.open()
+
+
+
 
 
 
@@ -71,11 +109,9 @@ class BladeLearner2049(App):
 
 
 
-
         def gui_normal_study():
             # close last pop 
             # setting parameters
-
 
 
             btn_return.bind(on_press= gui_study_type())
@@ -106,7 +142,6 @@ class BladeLearner2049(App):
             # update last pop 
             # setting a popup window on top for the page and buttons
 
-
             # Create a button that sets the parameter to "Hello"
             btn_yes = Button(root, text="Yes", command=lambda: mainStudy(self.chosen_category, 1) )
             btn_yes.pack()
@@ -131,32 +166,44 @@ class BladeLearner2049(App):
             value = 0
 
 
-        # build GUI background page
-        layout = FloatLayout(size=(600, 800), size_hint=(1, 1))
 
-        img_bg = AsyncImage(source='../images/1.jpg', pos_hint={'left': 1, 'top': 1}, size_hint=(1, 1), allow_stretch=True, keep_ratio=False)
-        layout.add_widget(img_bg)
-
-        bsh_x = 0.95
-        bsh_y = 0.9
-
-        x_pos = 0.5
-        y_pos = 0.5
 
         
 
+        # build GUI background page
+        layout = FloatLayout(size=(1200, 1600), size_hint=(1, 1))
+
+        bg = FloatLayout(size_hint=(1, 1))
+        address = '../images/image'+str(random.randint(1, 18))+'.jpg'
+        img_bg = AsyncImage(source=address, pos_hint={'center_x': 0.5, 'center_y': 0.5}, size_hint=(1, 1), allow_stretch=True, keep_ratio=False)
+        bg.add_widget(img_bg)
+        popup_bg = Popup(title=' ', content=bg, auto_dismiss=False, size_hint=(1, 1), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        popup_bg.open()
+
+        popup = Popup(title=' ', content=layout, auto_dismiss=False, size_hint=(1, 1), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        # create funtion to change color based on hour of day
+        color_pop = ((35 / 255.) + 0.5, (110 / 255.) + 0.5, (150 / 255.) + 0.5, 0.5)
+        popup.background_color = color_pop
+
+
+        gui_home()
+        return layout
+
+
+        
+        '''
         btn = Button(text='____________________________________________',
-                     
-                     background_color=(0.4, 0.4, 0.4, 0.93),
-                     color=(1, 1, 1, 0.9),
-                     size_hint=(bsh_x, bsh_y),
-                     pos_hint={'center_x': x_pos, 'center_y': y_pos})
+                    
+                    background_color=(0.4, 0.4, 0.4, 0.93),
+                    color=(1, 1, 1, 0.9),
+                    size_hint=(bsh_x, bsh_y),
+                    pos_hint={'center_x': x_pos, 'center_y': y_pos})
                     
 
         btn.bind(on_press=button_empty)
 
         layout.add_widget(btn)
-
+        '''
         return layout
 
 if __name__ == '__main__':
